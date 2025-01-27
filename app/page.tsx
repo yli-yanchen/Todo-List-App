dd9a0f01729876d43e68fb4a0a2422f5bae6208e
Cheryl Lee <yli.yanchen@gmail.com>
2025-01-27 15:49:02 -0800

    organize the folder structur

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,5 @@ import { useState } from 'react';
 import Image from 'next/image';
 
 export default function ToDoList() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewToDo] = useState([]);
-
-  return (
-    <main className=''>
-      <h1 className='flex justify-center align-top m-4 text-2xl font-bold '>
-        My To Do List
-      </h1>
-      {/* <div>
-        <input
-          type='text'
-          value={newTodo}
-          onChange={(e) => setNewToDo(e.target.value)}
-          placeholder='Add a new task'
-          className='p-2 border border-gray-300 rounded-md'
-        ></input>
-      </div> */}
-    </main>
-  );
+  return <main className=''></main>;
 }
