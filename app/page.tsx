2febf1a496a1882a129b6649b73da77dadd43a6a
Cheryl Lee <yli.yanchen@gmail.com>
2025-01-27 17:55:54 -0800

    organize the title as the layout

--- app/page.tsx
+++ app/page.tsx
@@ -32,13 +32,8 @@ export default function ToDoList() {
   };
 
   return (
-    <main className='h-screen flex flex-col bg-zinc-800'>
+    <>
       <section className='relative h-1/4 w-full bg-black flex justify-center items-center'>
-        <h1 className='font-bold font-sans text-white text-3xl'>
-          <span className='text-blue-400'>Todo </span>
-          <span className='text-purple-400'>App</span>
-        </h1>
-
         <div className='flex flex-row absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2 w-1/2 h-8 bg-blue-500 justify-center items-center border-2 border-transparent hover:border-yellow-200'>
           <p className='text-white text-sm font-semibold'>Create Task</p>
           <CiCirclePlus className='pl-2 size-6 text-white stroke-[1]' />
@@ -89,6 +84,6 @@ export default function ToDoList() {
           </div>
         </div>
       </section>
-    </main>
+    </>
   );
 }
